09932990e8afb8828f5f51d6dc2da0c3271b296b
Hayao Mori <hayaomori312@gmail.com>
2025-07-16 18:20:28 +0900

    initial commit and working progress

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar  from "./sections/Navbar.jsx"
+import Hero from "./sections/Hero.jsx"
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <Navbar />   
+      <Hero />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+
+      {/*about*/}
+      {/*projects*/}
+      {/*experience*/}
+      {/*testimonials*/}
+      {/*contact*/}
+      {/*footer*/}  
+
+      <div className='text-center text-2xl font-bold mt-10'>Welcome to the React 18 + Vite + Tailwind CSS + TypeScript Template</div>
+      App hello</div>
+  )
+}
+
+export default App
