a4b520ecc354dcd1bf45ea03766f9c104ea4eb26
Hayao Mori <hayaomori312@gmail.com>
2025-07-25 19:23:58 +0900

    refactoring & debugging

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,17 +1,32 @@
-import { CSS3DObject } from "three/examples/jsm/Addons.js";
 import { OrbitingCircles } from "./OrbitingCircles"
+import tailwindcssLogo from '../assets/logos/tailwindcss.svg';
+import css3Logo from '../assets/logos/css3.svg';
+import html5Logo from '../assets/logos/html5.svg';
+import javascriptLogo from '../assets/logos/javascript.svg';
+import railsPlainLogo from '../assets/logos/rails-plain.svg';
+import rubyOriginalLogo from '../assets/logos/ruby-original.svg';
+import bootstrapOriginalLogo from '../assets/logos/bootstrap-original.svg';
+
+const skillLogos = {
+    "tailwindcss": tailwindcssLogo,
+    "css3": css3Logo,
+    "html5": html5Logo,
+    "javascript": javascriptLogo,
+    "rails-plain": railsPlainLogo,
+    "ruby-original": rubyOriginalLogo,
+    "bootstrap-original": bootstrapOriginalLogo
+};
 
 export function Frameworks() {
-    const skills =["tailwindcss", "css3", "html5", "javascript", "rails-plain", "ruby-original", "bootstrap-original"
-
-    ];
+    const skills = Object.keys(skillLogos);
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
       <OrbitingCircles iconSize={40}>
-        {skills.map((skill, index) => (<Icon key={index} src={`src/assets/logos/${skill}.svg`} /> ))}
+        {skills.map((skill) => (<Icon key={skill} src={skillLogos[skill]} /> ))}
       </OrbitingCircles>
       <OrbitingCircles iconSize={25} radius={60} reverse speed={2}>
-      {skills.reverse().map((skill, index) => (<Icon key={index} src={`src/assets/logos/${skill}.svg`} /> ))}
+      {/* Create a reversed copy to avoid mutating the original array */}
+      {[...skills].reverse().map((skill) => (<Icon key={skill} src={skillLogos[skill]} /> ))}
       </OrbitingCircles>
     </div>
   );
