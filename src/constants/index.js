f56862786005d224566b6e627cc8ca752cc71858
Hayao Mori <hayaomori312@gmail.com>
2025-07-27 16:50:52 +0900

    testing

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,7 +25,7 @@ export const myProjects = [
       href: "",
       logo: "",
       code: 'https://github.com/HmoriKLTA/rails-wikinimous',
-      image: {wikinimousImg},
+      image: wikinimousImg,
       tags: [
         {
           id: 1,
